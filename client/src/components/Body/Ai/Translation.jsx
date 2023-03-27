92b29d672bc61a615bae4239f7cb8a7d459e378e
Debanjan Bandyopadhyay <19bcs2014@gmail.com>
2023-03-27 12:18:02 +0530

    added chat-feature

---
+++ client/src/components/Body/Ai/Translation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Translation({ doStuff, setInput, result }) {
+  return (
+    <>
+    
+    <div>
+      
+      <textarea
+        className="text-area"
+        cols={55}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>
+        ASK AI!
+      </button>
+
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+    </>
+  );
+}
--- client/src/components/Body/Message.js
+++ client/src/components/Body/Message.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Configuration, OpenAIApi } from "openai";
+import OptionSelection from './Ai/OptionSelection';
+import Translation from "./Ai/Translation";
+import { arrayItems } from "../AiOptions";
+import { useState } from "react";
+import './Ai/Message.css'
 
-export default function Message() {
+function Message() {
+
+  console.log(process.env.REACT_APP_Open_AI_Key)
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_Open_AI_Key
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  // console.log(import.meta.env.VITE_Open_AI_Key);
+  const selectOption = (option) => {
+    setOption(option);
+  };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    const response = await openai.createCompletion(object);
+
+    setResult(response.data.choices[0].text);
+  };
+
   return (
-    <div>
-      <div className="text-3xl">hi, This is Message Component</div>
+    <div className="App">
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result} />
+      )}
     </div>
   );
 }
+
+export default Message;
