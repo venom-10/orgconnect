fd602bf5cc75a0146b453a713d359ea1f9e6f131
venom-10 <19bcs2395@cuchd.in>
2023-05-17 23:00:37 +0530

    Fixing Chat.js

--- client/src/components/Body/Chat.js
+++ client/src/components/Body/Chat.js
@@ -1,44 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+import { ChatEngine, getOrCreateChat } from "react-chat-engine";
 
 const DirectChatPage = () => {
-	const [username, setUsername] = useState('')
+  const [username, setUsername] = useState("");
 
-	function createDirectChat(creds) {
-		getOrCreateChat(
-			creds,
-			{ is_direct_chat: true, usernames: [username] },
-			() => setUsername('')
-		)
-	}
+  function createDirectChat(creds) {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  }
 
-    function renderChatForm(creds) {
-		return (
-			<div>
-			  <div>
-				<input className="border-b border-t border-r border-l mt-1 rounded border-black"
-					placeholder='Username' 
-					value={username} 
-					onChange={(e) => setUsername(e.target.value)} 
-				/>
-				<button className="bg-orange-400 hover:bg-orange-500 font-bold rounded mx-2" onClick={() => createDirectChat(creds)}>
-					Create
-				</button>
-			</div>
-			</div>
-		)
-	}
+  function renderChatForm(creds) {
     return (
-		<ChatEngine
-			height='100vh'
-			userName='debanjan'
-			userSecret='12345'
-			projectID='
-            0dd38022-12ab-4047-a654-b8a6401f6b66'
-			renderNewChatForm={(creds) => renderChatForm(creds)}
-		/>
-	)
-}
+      <div>
+        <div>
+          <input
+            className="border-b border-t border-r border-l mt-1 rounded border-black"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button
+            className="bg-orange-400 hover:bg-orange-500 font-bold rounded mx-2"
+            onClick={() => createDirectChat(creds)}
+          >
+            Create
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <ChatEngine
+      height="100vh"
+      userName="debanjan"
+      userSecret="12345"
+      projectID="
+            58e49ac5-d6bc-4d0a-9e75-15d6b5000c0d"
+      renderNewChatForm={(creds) => renderChatForm(creds)}
+    />
+  );
+};
 
 export default DirectChatPage;
